451a6b0ccb59f4de234ab404991843fa9ea01142
Utkarsh_Pal <110682756+UttkarrshhPal@users.noreply.github.com>
2023-06-16 22:56:08 +0530

    Update max_Difference.cpp

--- max_Difference.cpp
+++ max_Difference.cpp
@@ -19,3 +19,27 @@ int main()
    cout<< max_diff(arr,n);
 
 }
+___________________________________________________________________________________________________________________________________________-
+   #include<iostream>
+using namespace std;
+int max_diff(int arr[],int n)
+{
+      // max_difference = arr[j]-min_Value se milega
+      //min_value = min(min_value,arr[j])
+      int res = arr[1]-arr[0];
+      int min_value = arr[0];
+      for(int j = 1;j<n;j++)
+      {
+        res = max(res,arr[j]-min_value);
+        min_value = min(min_value,arr[j]);
+
+      }
+      return res;
+}
+int main()
+{
+   int arr[]={2,3,10,6,4,8,1};
+   int n = 6;
+   cout<< max_diff(arr,n);
+
+}
