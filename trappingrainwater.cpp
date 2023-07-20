ab6af239aa85232d041292d35f533a12f5a7ad3a
Utkarsh_Pal <110682756+UttkarrshhPal@users.noreply.github.com>
2023-07-20 19:13:46 +0530

    Update trappingrainwater.cpp

--- trappingrainwater.cpp
+++ trappingrainwater.cpp
@@ -41,3 +41,47 @@ for(int i=1;i<n-1;i++)
 cout<<res;
 return 0;
 }
+----------------------------------------------------------------------------------------------------------------------------------------
+    
+#include<iostream>
+#include <cctype>
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <map>
+#include <iterator>
+#include <sstream>
+#include <iomanip>
+using namespace std;
+int main(){
+int arr[] = {5,0,0,2,0,4};
+int n = 6;
+int res = 0;
+int lmax[n];
+int rmax[n];
+
+lmax[0] = arr[0];
+for(int i=1;i<n-1;i++)
+{
+   lmax[i] = max(arr[i],lmax[i-1]);
+}
+
+rmax[n-1] = arr[n-1];
+for(int i = n-2;i>=0;i--)
+{
+    rmax[i] = max(arr[i],rmax[i+1]);
+}
+
+for(int i = 1;i<n-1;i++)
+res = res+(min(lmax[i],rmax[i])-arr[i]);
+
+
+cout<<res<<endl;
+}
+    
